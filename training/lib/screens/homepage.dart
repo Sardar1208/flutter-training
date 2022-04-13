d7460ca34dbc32afca8935e30c65ac8121844d34
sardar1208 <sarthaksbakre@192.168.1.2>
2022-04-13 18:09:40 +0530

    created a buisness card using Layouts knowledge

--- training/lib/screens/homepage.dart
+++ training/lib/screens/homepage.dart
@@ -7,7 +7,101 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyApp'),
+        title: const Text('Card using Layout'),
+      ),
+      body: Card(key: key),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey,
+      )),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.account_circle,
+                size: 60,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Flutter McFlutter",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    "Experienced App Developer",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "123 Main Street",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  "(415) 555-0198",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Icon(
+                Icons.accessibility,
+                size: 30,
+              ),
+              Icon(
+                Icons.timer,
+                size: 30,
+              ),
+              Icon(
+                Icons.phone_android,
+                size: 30,
+              ),
+              Icon(
+                Icons.phone_iphone,
+                size: 30,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
