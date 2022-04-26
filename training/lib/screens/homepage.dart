21b57803d67867a065839c7107aab39bd5558ac5
sardar1208 <sarthaksbakre@192.168.1.7>
2022-04-26 23:59:59 +0530

    created a app where ticker runs and then after 5sec redirect to new page

--- training/lib/screens/homepage.dart
+++ training/lib/screens/homepage.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:training/screens/new_page.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage>
+    with SingleTickerProviderStateMixin {
+  Duration _elapsed = Duration.zero;
+  late final Ticker _ticker;
+  @override
+  void initState() {
+    // print(_elapsed.compareTo(Duration(seconds: 0)));
+    super.initState();
+    _ticker = createTicker((elapsed) {
+      if (elapsed.compareTo(const Duration(seconds: 5)) > 0) {
+        _ticker.stop();
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const NewPage()));
+      }
+      setState(() {
+        _elapsed = elapsed;
+      });
+    });
+    _ticker.start();
+  }
+
+  @override
+  void deactivate() {
+    super.dispose();
+    print("disposed");
+    _ticker.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyApp'),
+        title: const Text('Lifecycle Methods'),
+      ),
+      body: Center(
+        child: Text(_elapsed.toString()),
       ),
     );
   }
